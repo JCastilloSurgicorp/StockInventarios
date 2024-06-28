4dfb7a843bee54a6318b2a8bf5f217406d44c1b8
JCastilloSurgicorp <jdcastillo@appsurgicorp.onmicrosoft.com>
2024-06-28 18:26:33 -0500

    Templates added

--- StockInventarioB/urls.py
+++ StockInventarioB/urls.py
@@ -3,4 +3,7 @@ from . import views as v
 
 urlpatterns = [
     path("", v.index, name="index"),
+    path("<int:question_id>/", v.detail, name="detail"),
+    path("<int:question_id>/results/", v.results, name="results"),
+    path("<int:question_id>/vote/", v.vote, name="vote"),
 ]
