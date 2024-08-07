154cc8ce76fa0abd23dbea7a541fba2e61e6d05c
administrador@SURGI.LOCAL <administrador@SURGI.LOCAL>
2024-08-07 12:21:44 -0500

    Server changes

--- DjangoSQLrestAPI/urls.py
+++ DjangoSQLrestAPI/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from rest_framework import routers
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
+#from debug_toolbar.toolbar import debug_toolbar_urls
 from rest_framework.documentation import include_docs_urls
 from StockInventarioB import views
 
@@ -40,4 +40,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', include_docs_urls(title='SI Api Documentation')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-] + debug_toolbar_urls()
+] #+ debug_toolbar_urls()
