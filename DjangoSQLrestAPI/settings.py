9d48a6ded2cce6c4aa6781d1b4451eb2dcd6fa20
JCastilloSurgicorp <jdcastillo@appsurgicorp.onmicrosoft.com>
2024-09-11 14:59:55 -0500

    static correction

--- DjangoSQLrestAPI/settings.py
+++ DjangoSQLrestAPI/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import mimetypes
 
-mimetypes.add_type("text/javascript", ".js", True)
+mimetypes.add_type("application/javascript", ".js", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,7 +139,7 @@ INTERNAL_IPS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
