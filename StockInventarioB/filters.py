8a36683f6e1e27b3eb1aac835b131cdf15f164cc
JCastillo <jdcastillo@appsurgicorp.onmicrosoft.com>
2024-12-04 17:29:11 -0500

    fixed filter

--- StockInventarioB/filters.py
+++ StockInventarioB/filters.py
@@ -10,13 +10,14 @@ class HojaPickingFilter(django_filters.FilterSet):
     class Meta:
         model = HojaPicking
         fields = {
-            'id':['iexact'],
-            'nro_guia':['iexact', 'icontains'],
-            'status_picking':['iexact'],
-            'lima_provincia':['iexact'],
-            'empr_id':['iexact'],
-            'tipo_pedido':['iexact'],
-            'atencion':['iexact', 'icontains'],
-            'fecha_atencion':['exact', 'lt', 'gt', 'range'],
-            'ubicacion_sector':['iexact', 'icontains'],
+            'id':['exact'],
+            'nro_guia':['exact', 'contains'], 
+            'status_picking':['exact'],
+            'lima_provincia':['exact'],
+            'empr_id':['exact'],
+            'tipo_pedido':['exact'],
+            'atencion':['exact', 'contains'], 
+            'fecha_atencion':['exact', 'lt', 'gt', 'range'], 
+            'ubicacion_sector':['exact', 'contains'],
         }
+        #fields = ['id', 'nro_guia',  'status_picking', 'lima_provincia', 'empr_id', 'tipo_pedido', 'atencion', 'fecha_atencion','ubicacion_sector']
