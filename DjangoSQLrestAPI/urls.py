cec586d2690ecb8363a4e1e3c3bf18dd46ddd849
JCastillo <jdcastillo@appsurgicorp.onmicrosoft.com>
2025-04-29 19:33:58 -0500

    api token verify

--- DjangoSQLrestAPI/urls.py
+++ DjangoSQLrestAPI/urls.py
@@ -31,5 +31,6 @@ urlpatterns = [
     path('api/', include('sharepoint_rest_api.urls', namespace='sharepoint')),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
     path('__debug__/', include('debug_toolbar.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
